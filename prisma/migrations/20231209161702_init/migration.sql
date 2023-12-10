233f57eb1be3813e51b1bfe83c72404483620c50
Angelo <angelo.machado@ufv.br>
2023-12-10 11:09:41 -0300

    feat: Create tables and relations

---
+++ prisma/migrations/20231209161702_init/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "State" (
+    "id" INTEGER NOT NULL,
+    "acronym" TEXT NOT NULL,
+
+    CONSTRAINT "State_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "City" (
+    "id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "idState" INTEGER NOT NULL,
+
+    CONSTRAINT "City_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CityHoliday" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "idCity" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CityHoliday_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "StateHoliday" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "idState" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "StateHoliday_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Holiday" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Holiday_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "City" ADD CONSTRAINT "City_idState_fkey" FOREIGN KEY ("idState") REFERENCES "State"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CityHoliday" ADD CONSTRAINT "CityHoliday_idCity_fkey" FOREIGN KEY ("idCity") REFERENCES "City"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StateHoliday" ADD CONSTRAINT "StateHoliday_idState_fkey" FOREIGN KEY ("idState") REFERENCES "State"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
