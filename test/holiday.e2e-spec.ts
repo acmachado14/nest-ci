9b7ed3ce0a646856a9dc9e5fc78f00a7f20f0b0d
Angelo <angelo.machado@ufv.br>
2023-12-10 12:35:03 -0300

    test: Add automated testing

--- test/holiday.e2e-spec.ts
+++ test/holiday.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { HolidayModule } from './../src/holiday.module';
 
-describe('AppController (e2e)', () => {
+describe('HolidayController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [HolidayModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -17,8 +17,10 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/feriados/31/2023-04-21')
       .expect(200)
-      .expect('Hello World!');
+      .expect({
+        name: 'Tiradentes',
+      });
   });
 });
