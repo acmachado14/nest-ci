98bd9553c157f7d6bcee36746383a41a9444a06e
Angelo <angelo.machado@ufv.br>
2023-12-10 12:59:43 -0300

    refactor: start waiting for seed

--- src/seeds/city.seed.ts
+++ src/seeds/city.seed.ts
@@ -2,31 +2,40 @@ import { createReadStream } from 'fs';
 import * as csv from 'csv-parser';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-
 async function seedCities() {
+  const prisma = new PrismaClient(); // Movido para dentro da função
+
   const filePath = 'municipios-2019.csv';
 
   const readStream = createReadStream(filePath);
 
-  readStream
-    .pipe(csv())
-    .on('data', async (row) => {
-      const idState = parseInt(row.codigo_ibge.substring(0, 2), 10);
-      const cityData = {
-        id: parseInt(row.codigo_ibge, 10),
-        name: row.nome,
-        idState: idState,
-      };
+  try {
+    await new Promise<void>((resolve, reject) => {
+      readStream
+        .pipe(csv())
+        .on('data', async (row) => {
+          const idState = parseInt(row.codigo_ibge.substring(0, 2), 10);
+          const cityData = {
+            id: parseInt(row.codigo_ibge, 10),
+            name: row.nome,
+            idState: idState,
+          };
 
-      await prisma.city.create({
-        data: cityData,
-      });
-    })
-    .on('end', () => {
-      console.log('Seed de cidades concluído com sucesso.');
-      prisma.$disconnect();
+          await prisma.city.create({
+            data: cityData,
+          });
+        })
+        .on('end', () => {
+          console.log('Seed de cidades concluído com sucesso.');
+          resolve();
+        })
+        .on('error', (error) => {
+          reject(error);
+        });
     });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 async function seed() {
@@ -34,8 +43,6 @@ async function seed() {
     await seedCities();
   } catch (error) {
     console.error('Erro ao executar a seed de feriados:', error);
-  } finally {
-    await prisma.$disconnect();
   }
 }
 
