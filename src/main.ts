4c1e25a4dc770d339ee799af9dd3916e730b2c80
Angelo <angelo.machado@ufv.br>
2023-12-10 11:16:39 -0300

    refactor: Add HolidayModule to app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HolidayModule } from './holiday.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(HolidayModule);
   await app.listen(3000);
 }
 bootstrap();
